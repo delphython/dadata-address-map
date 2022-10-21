9c88621613836c1ee586c604e99aa7a4afe97f2d
delphython <delphython@gmail.com>
2022-10-21 11:33:08 +0300

    add get_dadata_geocode_address view

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        "get_address/<str:source>/",
+        views.get_dadata_geocode_address,
+        name='get_address',
+    ),
+]
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,24 @@
+from dadata import Dadata
+from django.conf import settings
+from django.http import JsonResponse
 from django.shortcuts import render
 
+from .models import Addresses
+
 # Create your views here.
+
+def get_dadata_geocode_address(request, source):
+    dadata_token = settings.DADATA_TOKEN
+    dadate_secret = settings.DADATA_SECRET
+
+    with Dadata(dadata_token, dadate_secret) as dadata:
+        dadata_result = dadata.clean(name="address", source=source)
+
+    return JsonResponse(
+        dadata_result,
+        safe=False,
+        json_dumps_params={
+            "ensure_ascii": False,
+            "indent": 4,
+        },
+    )
